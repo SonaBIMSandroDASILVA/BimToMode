ff00bc95d0826c341d435a9dfdc1d484b1a72c02
SonaBIMSandroDASILVA <sandro@sonabim.com>
2025-10-16 14:34:52 +0200

    Version finale

--- BimToMode/App.cs
+++ BimToMode/App.cs
@@ -1,34 +1,49 @@
 ﻿using Autodesk.Revit.UI;
 using System;
+using System.IO;
+using System.Reflection;
 using System.Windows;
-
-
+using System.Windows.Media.Imaging;
 
 namespace BimToMode
 {
     public class App : IExternalApplication
     {
+        private string EmbeddedParentIconLocation => GetType().Namespace + ".Ressources.Icons.Embedded.";
+        string name = string.Empty;
+        string text = string.Empty;
+        string toolTip = string.Empty;
+        string largeImageName = string.Empty;
+
         public Result OnStartup(UIControlledApplication application)
         {
             try
             {
                 // Création d'un onglet personnalisé dans le ruban Revit
-                string tabName = "BimToMode";
+                string tabName = "ScanToBIM";
                 application.CreateRibbonTab(tabName);
 
                 // Création d'un panneau dans cet onglet
                 RibbonPanel panel = application.CreateRibbonPanel(tabName, "Modélisation à partir d'un nuage de points");
 
+                name = "btnOpenWindow"; // Nom technique du bouton
+                text = "ScanToBIM"; // Texte affiché sur le bouton
+                toolTip = "Modélisation à partir d'un nuage de points au format e57"; // Info-bulle du bouton (s'affiche quand la souris est sur le bouton)
+                largeImageName = EmbeddedParentIconLocation + "bouton.png"; // Chemin de l'image de l'icône du bouton
+
                 // Bouton pour ouvrir la fenêtre principale
                 PushButtonData buttonData = new PushButtonData(
-                    "btnOpenWindow", 
-                    "ScanToBIM", 
+                    name,
+                    text,
                     GetType().Assembly.Location,
-                    "BimToMode.CommandStart"
-                );
+                    "BimToMode.CommandStart")
+                {
+                    ToolTip = toolTip,
+                    LargeImage = GetEmbeddedImage(largeImageName)
+                };
 
                 // Ajout du bouton au panneau
-                panel.AddItem(buttonData);
+                PushButton? btnOpen = panel.AddItem(buttonData) as PushButton;
 
                 return Result.Succeeded;
             }
@@ -42,6 +57,23 @@ namespace BimToMode
         public Result OnShutdown(UIControlledApplication application)
         {
             return Result.Succeeded;
+        }
+
+        private BitmapSource? GetEmbeddedImage(string name)
+        {
+            try
+            {
+                // Charge l'image depuis les ressources du projet
+                Assembly a = Assembly.GetExecutingAssembly(); // Récupère l'assemblage (le projet en cours)
+                Stream? s = a.GetManifestResourceStream(name); // Trouve l'image dans les ressources
+                if (s == null)
+                    return null;
+                return BitmapFrame.Create(s); // Crée une image bitmap à partir du flux de données
+            }
+            catch
+            {
+                return null; // Si l'image n'est pas trouvée ou qu'il y a une erreur, retourne null
+            }
         }
     }
 }
